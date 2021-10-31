3abd5ae2d5bc66913e6a4ce43344c0224b7130a7
thommccarthy <mccarttj@gmail.com>
2021-10-31 17:10:18 -0400

    setup basic migrations and db connection, add navbar and basic styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,21 @@
     <title>Posty</title>
 </head>
 
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white"></nav>
+<body class="bg-gray-300">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Dashboard</a></li>
+            <li><a href="" class="p-3">Posts</a></li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Thom McCarthy</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
