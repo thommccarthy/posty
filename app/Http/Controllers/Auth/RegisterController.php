9cd7b369908776d84701dd7059193fc7f781ca51
thommccarthy <mccarttj@gmail.com>
2021-11-02 10:25:13 -0400

    validate registration form and redirect user to a dashboard route

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,8 +14,33 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    //request object pulled in at top of file
+    public function store(Request $request)
     {
-        dd('abc');
+        //validation
+        $this->validate(
+            $request,
+            [
+                'name' => 'required|max:255',
+                'username' => 'required|max:255',
+                'email' => 'required|email|max:255',
+                //looks for the other form field with 'confirmation' in name
+                'password' => 'required|confirmed'
+
+            ]
+
+        ); //throws error if not validated, built into laravel
+        //store user
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+
+        ]);
+        //sign the user in
+        //redirect
+        return redirect()->route('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 // import controllers
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 // for controllers, specify route then array which contains Controller namespace, followed by method. in this instance RegisterController's index method returns the view in auth.register
 
 // name is chained on at end, this way in case the path changes the reference will remain the same without needing to change the code that is calling the route
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
